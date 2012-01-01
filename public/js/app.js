8bafcd9c5355019b98f4bf00715fa041fbe0dbfa
Ed Summers <ehs@pobox.com>
2011-12-31 21:42:18 -0500

    no longer need this since it si only happening once

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,6 @@ $(document).ready(init);
 function init() {
     var socket = io.connect();
     socket.on('connect', function() {
-        alert('connected'); 
         socket.on('firehose', function(msg) {
             addUpdate(msg);
             removeOld();
